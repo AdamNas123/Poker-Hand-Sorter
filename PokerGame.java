fc57abc429ee45732d62b12b6d60a8c72e4ddf63
Adam Nasser <adam.nasser246@gmail.com>
2025-05-28 16:03:57 +1000

    add initial planning comments in main function

--- PokerGame.java
+++ PokerGame.java
@@ -1,6 +1,28 @@
 public class PokerGame {
 
     public static void main(String[] args) {
+    //First, read in hands and save them to player 1 and player 2 objects
 
+    //Then, loop through each player's hands
+        //Because of tied ranking cases, will need to consider the full hand
+        //and give some sort of number ranking per hand
+
+        /*There are some base cases that each hand can fall in:
+         * 1) High Card (1)
+         * 2) Same Values:
+         *      - Pair (2)
+         *      - Two Pairs (3)
+         *      - 3 of a kind (4)
+         *      - full house (7)
+         *      - four of a kind (8)
+         *  3) Same Suit:
+         *      - Flush (6)
+         *      - Straight Flush (9) (Combo of 3 and 4 cases)
+         *      - Royal Flush (10) (Combo of 3, 4 with specific cards)
+         *  4) Order:
+         *      - Straight (5)
+         *      - Straight Flush (9) (Combo of 3 and 4 cases)
+         *      ~ Maybe Royal Flush (Combo of 3, 4 with specific cards)
+        */
     }
 }
