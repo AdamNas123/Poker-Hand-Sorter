097f0238e19161e4e562c04cbd24ea233a124848
Adam Nasser <adam.nasser246@gmail.com>
2025-05-28 20:16:36 +1000

    add hashmaps to count card ranks and suits in a players hand

--- Player.java
+++ Player.java
@@ -4,14 +4,13 @@ import java.util.*;
  * Represents the player, their list of cards (hand) and number of wins
  */
 public class Player {
-    private int playerNumber;
+    private final int playerNumber;
     private List<Card> hand;
     private int handScore = 0; //A total score of the hand required for tie-breaking cases
     private int winCount = 0;
 
-    public Player(int playerNumber, List<Card> hand) {
+    public Player(int playerNumber) {
         this.playerNumber = playerNumber;
-        this.hand = new ArrayList<>(hand);
     }
 
     //Overridden to string function to display player fields
@@ -36,8 +35,26 @@ public class Player {
         return winCount;
     }
 
+    public void createHand(List<Card> hand) {
+        this.hand = new ArrayList<>(hand);
+    }
 
     public int rankHand() {
+        //Start by sorting hand into both hashmap of ranks and then suits
+        HashMap<Integer, Integer> ranks = new HashMap<Integer, Integer>();
+        HashMap<Character, Integer> suits = new HashMap<Character, Integer>();
 
+        //Sort hand into the ranks and suits hashmaps
+        for (Card card : this.hand) {
+            ranks.merge(card.getRankValue(), 1, Integer::sum);
+            suits.merge(card.getSuit(), 1, Integer::sum);
+        }
+        System.out.println("Player " + playerNumber + " Ranks: " + ranks.toString());
+        System.out.println("Player " + playerNumber + " Suits: " + suits.toString());
+
+        //For ranks -> Determine if hand is 4 of a kind, full house, 3 of a kind, 2 pair or pair
+        //For suits -> Determine if hand is royal flush or flush
+        // Straight -> Straight or straight flush
+        return 0;
     }
 }
