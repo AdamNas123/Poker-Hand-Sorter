7579c647d97ad8f2c8022c036e81b25631a476a6
Adam Nasser <adam.nasser246@gmail.com>
2025-05-28 21:33:35 +1000

    add checks for repeated card values (four and three of a kinds and pairs)

--- Player.java
+++ Player.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class Player {
     private final int playerNumber;
     private List<Card> hand;
+    private HandRank handRank;
     private int handScore = 0; //A total score of the hand required for tie-breaking cases
     private int winCount = 0;
 
@@ -20,6 +21,7 @@ public class Player {
                 "playerNumber='" + playerNumber + '\'' +
                 ", hand='" + hand + '\'' +
                 ", handScore='" + handScore + '\'' +
+                ", handRank='" + handRank + '\'' +
                 ", winCount='" + winCount + '\'' +
                 '}'
                 ;
@@ -53,8 +55,40 @@ public class Player {
         System.out.println("Player " + playerNumber + " Suits: " + suits.toString());
 
         //For ranks -> Determine if hand is 4 of a kind, full house, 3 of a kind, 2 pair or pair
+        countRanks(ranks);
+        System.out.println("Player " + playerNumber + " HandRank: " + handRank.toString());
         //For suits -> Determine if hand is royal flush or flush
         // Straight -> Straight or straight flush
         return 0;
+    }
+
+    private void countRanks(HashMap<Integer, Integer> ranks) {
+        if (ranks.containsValue(4)) {
+            this.handRank = HandRank.FOUR_OF_A_KIND;
+        }
+        else if (ranks.containsValue(3) && ranks.containsValue(2)) {
+            this.handRank = HandRank.FULL_HOUSE;
+        }
+        else if (ranks.containsValue(3)) {
+            this.handRank = HandRank.THREE_OF_A_KIND;
+        }
+        //Loop through the ranks hashmap to check if there is either a two pair or pair
+        else {
+            int pairs = 0;
+            for (int rankCount : ranks.values()) {
+                if (rankCount == 2) {
+                    ++pairs;
+                }
+            }
+            if (pairs == 2) {
+                this.handRank = HandRank.TWO_PAIRS;
+            }
+            else if (pairs == 1) {
+                this.handRank = HandRank.PAIR;
+            }
+            else {
+                this.handRank = HandRank.HIGH_CARD;
+            }
+        }
     }
 }
